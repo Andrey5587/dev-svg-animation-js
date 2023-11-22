4fe1231ad39167f8c01c69786fddf61f6332f6d7
Andrey5587 <andreyzp1977@gmail.com>
2023-11-22 22:02:46 +0200

    feat: creat animation-js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,47 @@
-const animationPath = document.querySelectorAll('.animation-path')
-const buttons = document.querySelectorAll('.btn')
+const installAnimatedPanel = () => {
+    const animationPath = document.querySelectorAll('.animation-path')
+    const buttons = document.querySelectorAll('.btn')
 
-//const removeActiveClass = (selector) => selector.forEach(item => {
-//    item.classList.remove('active')
-//})
-//removeActiveClass(buttons)
- function removeActiveClass() {
-    buttons.forEach (item => {
-        item.classList.remove('active')
+
+    //const removeActiveClass = (selector) => selector.forEach(item => {
+    //    item.classList.remove('active')
+    //})
+    //removeActiveClass(buttons)
+
+
+    function removeActiveClass() {
+        buttons.forEach(item => {
+            item.classList.remove('active')
+        })
+
+        animationPath.forEach(path =>{
+            path.classList.remove('show')
+        })
+    }
+
+
+    const changeCase = (event) =>{
+        
+        removeActiveClass();
+        event.target.classList.add('active')
+
+        const getBtnCase = event.target.getAttribute('data-animation');
+        const onShowAnimationPath = document.querySelector("#" + getBtnCase);
+        const animated = document.querySelector("image")
+        const animatedClassNow = animated.getAttribute('class')
+
+
+        animated.classList.replace(animatedClassNow, 'animation-' + getBtnCase)
+        onShowAnimationPath.classList.add('show')
+
+        console.log(getBtnCase);
+    }
+
+    buttons.forEach(btn => {
+        btn.addEventListener('click', changeCase)
     })
- }
- //removeActiveClass()
 
-buttons.forEach(btn => {
-  btn.addEventListener('click', () => {
-    removeActiveClass()
-    btn.classList.add('active') 
-})
-})
 
-animationPath.forEach(path => {
-    console.log(path);
-})
+}
+
+document.querySelector('.panel') ? installAnimatedPanel() : null;
